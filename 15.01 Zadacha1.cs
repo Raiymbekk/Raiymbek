da086a71bde773a9cfad161adc37e238bf30d74b
Raiymbekk <63302092+Raiymbekk@users.noreply.github.com>
2024-01-29 22:06:22 +0600

    15.01

---
+++ 15.01 Zadacha1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class mathematics
+
+{
+    //int
+    public int Сложение(int a, int b)
+    {
+        return a + b;
+    }
+
+    //double
+    public double Сложение(double a, double b)
+    {
+        return a + b;
+    }
+
+    //конкатенация строк
+    public string Сложение(string a, string b)
+    {
+        return a + b;
+    }
+}
+
+class Primer1
+{
+    static void Main()
+    {
+        mathematics математика = new mathematics();
+
+        // Использование метода сложения для целых чисел (int)
+        int результатInt = математика.Сложение(5, 10);
+        Console.WriteLine("Сложение int: " + результатInt);
+
+        // Использование метода сложения для чисел с плавающей точкой (double)
+        double результатDouble = математика.Сложение(3.5, 2.7);
+        Console.WriteLine("Сложение double: " + результатDouble);
+
+        // Использование метода сложения для строк (конкатенация строк)
+        string результатString = математика.Сложение("Привет, ", "мир!");
+        Console.WriteLine("Сложение string: " + результатString);
+    }
+}
